50230ba5fd5fc64b8a87240e20ccfce4f658ad46
Max Sehaumpai <msehaumpai@gmail.com>
2022-07-10 18:41:37 -0400

    Efaz teaching Max js and css - homepage boxes

---
+++ src/components/QuoteBox.js
@@ -0,0 +1,19 @@
+import "./QuoteBox.css"
+
+const QuoteBox = () => {
+    return (
+        <>
+            <div className="quote-container"> 
+                <p className="name">Max Sehaumpai</p>
+                <p className="quote">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                {/* <p className="quote">If it's not broken, don't fix it.</p> */}
+                <div className="book-info">
+                    <p className="book-title">The Lighting Thief</p>
+                    <p className="book-author">Rick Riordan</p>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default QuoteBox
--- src/pages/HomePage/Home.js
+++ src/pages/HomePage/Home.js
@@ -1,7 +1,7 @@
-import { confirmPasswordReset } from "firebase/auth";
 import React from "react";
 import { useAuth } from "../../firebase";
-
+import QuoteBox, {cat} from "../../components/QuoteBox";
+import "./Home.css"
 
 
 const Home = () => {
@@ -9,10 +9,17 @@ const Home = () => {
     console.log(currentUser);
 
     return (
-        <>
+        <div className="home-container">
             <p>this is {currentUser?.email || "stranger"} home page</p>
             <p>User id : {currentUser?.uid || "no UID"}</p>
-        </>
+
+            <div className="quote-boxes">
+                <QuoteBox/> 
+                <QuoteBox/>
+                <QuoteBox/>
+                <QuoteBox/>
+            </div>
+        </div>
     )
 }
 
