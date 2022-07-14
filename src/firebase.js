84768f283706c2241ef1a46026b2e60972fb41f1
ritvikbanakar <ritvikbanakar15@gmail.com>
2022-07-14 15:21:46 -0500

    Read from Firebase and Display Quotes on Home Page

--- src/firebase.js
+++ src/firebase.js
@@ -3,23 +3,12 @@ import { initializeApp } from "firebase/app";
 // import { getAnalytics } from "firebase/analytics";
 import { getAuth, createUserWithEmailAndPassword, onAuthStateChanged, signOut, signInWithEmailAndPassword } from "firebase/auth";
 import { useEffect, useState } from "react";
-// import { firebaseConfig } from "./firebase.config";
+import { firebaseConfig } from "./firebase.config.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-
-const firebaseConfig = {
-    apiKey: "AIzaSyB9_3cFjNhPQsp5j6aohMoEWxgTU-E2_ps",
-    authDomain: "quotable-a0bf3.firebaseapp.com",
-    projectId: "quotable-a0bf3",
-    storageBucket: "quotable-a0bf3.appspot.com",
-    messagingSenderId: "712734492453",
-    appId: "1:712734492453:web:72da11a7cc1faf2612fccc",
-    measurementId: "G-ET998LDRNR"
-  };  
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
--- src/pages/HomePage/Home.js
+++ src/pages/HomePage/Home.js
@@ -1,34 +1,70 @@
-import React from "react";
+import React, {useEffect, useRef, useState} from "react";
 import { useAuth } from "../../firebase";
 import { getDatabase, ref, child, get } from "firebase/database";
-
-var books = []
-var quotes = []
-
-function readQuotes(userId) {
-    const dbRef = ref(getDatabase());
-    get(child(dbRef, `users/${userId}`)).then((snapshot) => {
-      if (snapshot.exists()) {
-        snapshot.forEach((child) => {
-            books.push(child.key)
-            console.log(books)
-        });
-      } else {
-        console.log("No data available");
-      }
-    }).catch((error) => {
-      console.error(error);
-    });
-}
-
+import Card from '@mui/material/Card';
+import {Button, CardActions, CardContent, Typography} from "@mui/material";
 
 const Home = () => {
+    const[books, setBooks] = useState([])
     const currentUser = useAuth();
-    readQuotes(currentUser?.email.split("@")[0])
-   
+    const dbRef = ref(getDatabase());
+    // Book: Highlights
+    // String: Array[String]
+    let bookMap = useRef(new Map())
+    // Reverse required to have the cards by quotes rather than by book
+    let [reverse,setReverse] = useState([])
+    useEffect(()=> {
+        get(child(dbRef, `users/${currentUser?.email.split("@")[0]}`)).then((snapshot) => {
+            if (snapshot.exists()) {
+                setBooks([...books, Object.keys(snapshot.val())])
+                for(const [key1, val1] of Object.entries(snapshot.val())){
+                    let ret = []
+                   for(const index of Object.values(val1)){
+                       ret.push(index)
+                   }
+                   for(const high of ret){
+                       reverse.push([high, key1])
+
+                   }
+                    setReverse(reverse)
+                    bookMap.current.set(key1, ret)
+                }
+                console.dir(Object.values(snapshot.val()))
+                console.dir(bookMap.current)
+
+            } else {
+                console.log("No data available");
+            }
+        }).catch((error) => {
+            console.error(error);
+        });
+    }, [currentUser?.email, dbRef, reverse])
+
     return (
-        <p>this is {currentUser?.email || "stranger"} home page</p>
-    )
-}
+      <div className="App">
+          {
+           reverse.map((key)=> {
+                return (
+                    <Card key ={key[1]+"-"+key[0].highlight} sx={{ minWidth: 275, margin:"15px" }}>
+                        <CardContent>
+                            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                                {key[1]+""}
+                            </Typography>
+                            <Typography variant="h5" component="div">
+                                {key[0].highlight}
+                            </Typography>
+                        </CardContent>
+                        <CardActions>
+                            <Button size="small" onClick={(e)=>{
+                                window.alert("pressed" + e.target)
+                            }}>Learn Less</Button>
+                        </CardActions>
+                    </Card>
+                )
+            })
+        }
+      </div>
+    );
+  }
 
 export default Home;
